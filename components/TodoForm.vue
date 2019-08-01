5e60b236c4fda7966695ed40a49c2ff537dc1764
田代拓也 <tashitaku@tashiro-Mac-mini.local>
2019-08-01 12:27:21 +0900

    :+1: basebuttonのcomponentを作成 #1

--- components/TodoForm.vue
+++ components/TodoForm.vue
@@ -44,6 +44,18 @@
     >
       <label slot="label" class="text-white px-1">期限</label>
     </BaseInput>
+    <div class="flex flex-col justify-start items-start">
+      <BaseButton
+        class="h-8 w-12 bg-blue-600 flex p-1 items-stretch"
+      >
+        追加
+      </BaseButton>
+      <BaseButton
+        class="h-8 w-14 bg-gray-500 flex p-1 items-stretch"
+      >
+        クリア
+      </BaseButton>
+    </div>
   </div>
 </template>
 
@@ -52,12 +64,14 @@ import { Vue, Component } from 'nuxt-property-decorator'
 import BaseInput from '@/components/BaseInput.vue'
 import BaseRadioGroup from '@/components/BaseRadioGroup.vue'
 import BaseRadio from '@/components/BaseRadio.vue'
+import BaseButton from '@/components/BaseButton.vue'
 
 @Component({
   components: {
     BaseInput,
     BaseRadioGroup,
-    BaseRadio
+    BaseRadio,
+    BaseButton
   },
   computed: {
     items (): object[] {
