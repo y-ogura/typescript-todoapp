a9846ae6edc7d959930bb38035ff9f11c4d872ee
山岸　大地 <yamagishidaichi@yamagishidaichinoMac-mini.local>
2019-08-01 11:45:37 +0900

    TheHeader作成 #2

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="title">
-        typescript-todoapp
-      </h1>
       <TodoForm />
     </div>
   </div>
@@ -36,16 +33,6 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
 }
 
 .subtitle {
