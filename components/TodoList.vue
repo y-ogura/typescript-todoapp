0a8ea37979344cfa9b407ea9a04bb637e0170929
d-yamagishi522 <d-yamagishi@gstaff.co.jp>
2019-08-01 15:22:57 +0900

    TodoListコンポーネント作成 #7

---
+++ components/TodoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div v-for="item in todoList" :key="item.id">
+      <todo-list-item :item="item" class="mt-2" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import TodoListItem from '@/components/TodoListItem.vue'
+interface Task {
+  id: string,
+  name: string,
+  priority: string,
+  due: string,
+  message: string,
+  isDone: boolean
+}
+@Component({
+  components: {
+    TodoListItem
+  }
+})
+export default class TodoList extends Vue {
+  todoList: Task[] = [
+    {
+      id: '1',
+      name: 'taskName1',
+      priority: '高',
+      due: '2019-08-01',
+      message: '【今日が期限です】',
+      isDone: false
+    },
+    {
+      id: '2',
+      name: 'taskName2',
+      priority: '中',
+      due: '2019-08-01',
+      message: '【今日が期限です】',
+      isDone: false
+    }
+  ]
+}
+</script>
--- components/TodoListItem.vue
+++ components/TodoListItem.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="border rounded">
     <div class="border-b font-bold p-1">
-      {{ task.name }}
+      {{ item.name }}
     </div>
     <div class="flex justify-between bg-gray-100 p-1">
       <div class="w-1/2 text-sm">
         <span class="font-bold">優先度</span>
-        <span class="mr-3">{{ task.priority }}</span>
+        <span class="mr-3">{{ item.priority }}</span>
         <span class="font-bold">期限</span>
-        <span class="mr-2">{{ task.due }}</span>
-        <span>{{ task.message }}</span>
+        <span class="mr-2">{{ item.due }}</span>
+        <span>{{ item.message }}</span>
       </div>
       <div class="w-1/2 flex justify-end items-center">
         <input type="checkbox" class="mr-2" />
@@ -24,16 +24,18 @@
 </template>
 
 <script lang="ts">
-import { Vue, Prop } from 'nuxt-property-decorator'
+import { Vue, Prop, Component } from 'nuxt-property-decorator'
 interface Task {
+  id: string,
   name: string,
   priority: string,
   due: string,
   message: string,
   isDone: boolean
 }
+@Component
 export default class TodoListItem extends Vue {
-  @Prop({ default: {} })
-  task!: Task
+  @Prop({ type: Object, default: {} })
+  item!: Task
 }
 </script>
