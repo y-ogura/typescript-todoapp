7f68760dbacf1df2890d893cda15d9bc0c3a4ae8
d-yamagishi522 <d-yamagishi@gstaff.co.jp>
2019-08-01 17:00:23 +0900

    BaseButton追加 #8

--- components/BaseModal.vue
+++ components/BaseModal.vue
@@ -6,17 +6,18 @@
           <div>
             {{ modalTitle }}
           </div>
-          <button class="close-button" @click="$emit('closeModal')">
+          <button class="outline-none" @click="$emit('closeModal')">
             ×
           </button>
         </div>
         <slot />
-        <!-- Todo: ボタンをBaseButtonに変更 -->
         <div class="px-6 py-2 flex justify-end border-t">
-          <button class="mr-4">
+          <base-button class="mr-4 h-8 w-14 bg-blue-500 flex p-1 items-stretch rounded" @click="$emit('isDone')">
             完了
-          </button>
-          <button>クリア</button>
+          </base-button>
+          <base-button class="h-8 w-14 bg-gray-500 flex p-1 items-stretch rounded" @click="$emit('clear')">
+            クリア
+          </base-button>
         </div>
       </div>
     </div>
@@ -25,7 +26,12 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'nuxt-property-decorator'
-@Component
+import BaseButton from '@/components/BaseButton.vue'
+@Component({
+  components: {
+    BaseButton
+  }
+})
 export default class BaseModal extends Vue {
   @Prop({ type: String, default: '' })
   modalTitle!: String
@@ -40,7 +46,7 @@ export default class BaseModal extends Vue {
 .bg-color-gray {
   background-color: rgba(155, 147, 147, 0.5)
 }
-.close-button:focus {
+.outline-none:focus {
   outline:0;
 }
 </style>
